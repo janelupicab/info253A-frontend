14caf4bc4ca1c4d71fe37b41bb20c29e0d8b82b7
jane lupica <jane_lupica@berkeley.edu>
2022-10-09 11:33:34 -0700

    lab5

---
+++ lab5/lab-5/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Landing from './myComponent/landing';
+import './myComponent/landing.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Landing/>
+    </>
+);
